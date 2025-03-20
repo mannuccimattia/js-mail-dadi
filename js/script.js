d43e1de25cee0d4ec5008c659787f6aea850047b
mannuccimattia <mannuccimattia@gmail.com>
2025-03-20 15:39:24 +0100

    [UPD] Done script.js mail array and prompt

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* ******************************************** */
+/*                     MAIL                     */
+/* ******************************************** */
+
+// definisco un array contenente la lista delle email degli invitati
+const guestsMail = [
+  'mariorossi@mail.com',
+  'pieroverdi@mail.com',
+  'lucaneri@mail.com',
+  'silviorossi@mail.com',
+  'bianchiantonio@mail.com',
+  'giorgiobianchi@mail.com',
+  'giovannimuciaccia@mail.com',
+]
+
+// chiedo all'utente di inserire un indirizzo email
+let userMail = prompt("Inserisci il tuo indirizzo email");
+// stampo l'indirizzo email dell'utente
+console.log("Il tuo indirizzo email è ", userMail);
+
